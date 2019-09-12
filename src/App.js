8a93976ee5dbc38c06212b038989158164b27c42
Dante Garin <dantegarin@gmail.com>
2019-09-12 12:32:20 -0300

    * ADD: Final Commit, end of the course

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, {useState} from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
+import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register';
 import FaceRecon from './components/FaceRecon/FaceRecon';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import './App.css';
@@ -27,24 +29,98 @@ const particlesOptions = {
 function App() {
   const [input, setInput] = useState('');
   const [box, setBox] = useState({});
-  let onButtonSubmit = () => {
-    console.log("click");
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, input)
-    .then(response => setBox(response))
+  const [route, setRoute] = useState('signin');
+  const [isSignedIn, setSignedIn] = useState(false);
+  const [user, setUser] = useState({
+    id: '',
+    name: '',
+    email: '',
+    entries: 0,
+    joined: ''
+  });
+  const [imageUrl, setImageURL] = useState('');
+
+  let onPictureSubmit = () => {
+    console.log("submit");
+    setImageURL(input);
+    app.models
+    .predict(
+      Clarifai.FACE_DETECT_MODEL, 
+      imageUrl)
+    .then(response => 
+      {
+        if (response) {
+          fetch('http://localhost:3002/image', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+              body: JSON.stringify({
+                  id: user.id
+              })
+          })
+          .then(response => response.json())
+          .then(count => {
+            setUser({...user, entries: count});
+          })
+        };
+        console.log(response);
+        displayFaceBox(calculateFaceLoc(response));
+      }
+    ); 
   };
 
   let calculateFaceLoc = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
 
+  let displayFaceBox = (box) => {
+    setBox(box); 
+  }
+
+  let loadUser = (data) =>{
+    console.log(data);
+    setUser({
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined
+    });
+  }
+
+  let onRouteChange = (where) => {
+    if (where === 'signout'){
+      setSignedIn(false);
+    } else if (where === 'home'){
+      setSignedIn(true);
+    }
+    setRoute(where);
   }
 
   return (
     <div className="App">
       <Particles className="particles" params={particlesOptions} />
-      <Navigation></Navigation>
+      <Navigation isSignedIn={isSignedIn} onRouteChange={onRouteChange}></Navigation>
+      { route === 'home' 
+      ? <div> 
       <Logo></Logo>
-      <Rank></Rank>
-      <ImageLinkForm value={input} onInputChange={e => setInput(e.target.value)} onButtonSubmit={onButtonSubmit}></ImageLinkForm>
-      <FaceRecon imageUrl={input}></FaceRecon>
+      <Rank name={user.name} entries={user.entries}></Rank>
+      <ImageLinkForm value={input} onInputChange={e => setInput(e.target.value)} onPictureSubmit={onPictureSubmit}></ImageLinkForm>
+      <FaceRecon box={box} imageUrl={input}></FaceRecon>
+      </div> 
+      : (
+        route === 'signin'
+        ? <Signin onRouteChange={onRouteChange} loadUser={loadUser}/>
+        : <Register onRouteChange={onRouteChange} loadUser={loadUser}/>)
+      }
     </div>
   );
 }
